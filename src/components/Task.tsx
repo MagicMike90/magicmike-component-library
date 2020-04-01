b98e27595b8568094c3e97727d3d43aff8d13f82
Michael Luo <michael.luo0801@gmail.com>
2020-04-01 22:32:08 +1100

    add types to TaskView

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React from 'react';
+import { Task } from './types';
 
-export default function Task({
+interface Props {
+  task: Task;
+  onArchiveTask: () => void;
+  onPinTask: () => void;
+}
+const TaskView: React.FC<Props> = ({
   task: { id, title, state },
   onArchiveTask,
   onPinTask
-}) {
+}) => {
   return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+        />
+      </div>
+
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
     </div>
   );
-}
+};
+export default TaskView;
--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { action } from "@storybook/addon-actions";
+import React from 'react';
+import { action } from '@storybook/addon-actions';
 
-import Task from "../components/Task";
+import TaskView from '../components/Task';
 
 // To tell Storybook about the component we are documenting
 export default {
-  component: Task,
-  title: "Task",
+  component: TaskView,
+  title: 'Task',
   // Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/
 };
 
 //  (taskData) to build out the shape of the task the component expects
 export const taskData = {
-  id: "1",
-  title: "Test Task",
-  state: "TASK_INBOX",
+  id: '1',
+  title: 'Test Task',
+  state: 'TASK_INBOX',
   updatedAt: new Date(2018, 0, 1, 9, 0)
 };
 
@@ -23,16 +23,18 @@ export const taskData = {
 // when clicked. So when we build a pin button,
 // we’ll be able to determine in the test UI if a button click is successful.
 export const actionsData = {
-  onPinTask: action("onPinTask"),
-  onArchiveTask: action("onArchiveTask")
+  onPinTask: action('onPinTask'),
+  onArchiveTask: action('onArchiveTask')
 };
 
-export const Default = () => <Task task={{ ...taskData }} {...actionsData} />;
+export const Default = () => (
+  <TaskView task={{ ...taskData }} {...actionsData} />
+);
 
 export const Pinned = () => (
-  <Task task={{ ...taskData, state: "TASK_PINNED" }} {...actionsData} />
+  <TaskView task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />
 );
 
 export const Archived = () => (
-  <Task task={{ ...taskData, state: "TASK_ARCHIVED" }} {...actionsData} />
+  <TaskView task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
 );
